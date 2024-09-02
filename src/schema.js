0ec0d4dd736c9aee14dfc2c4985ebf875179e5e2
Mayank Kush <115919938+Mayank-wen@users.noreply.github.com>
2024-09-02 18:24:49 +0530

    Create schema.js

---
+++ src/schema.js
@@ -0,0 +1,20 @@
+const { gql } = require('apollo-server-express');
+module.exports = gql`
+ type Note {
+ id: ID!
+ content: String!
+ author: String!
+ createdAt: DateTime!
+ updatedAt: DateTime!
+
+ }
+ type Query {
+ notes: [Note!]!
+ note(id: ID!): Note!
+ }
+ type Mutation {
+    newNote(content: String!): Note!
+    updateNote(id: ID!, content: String!): Note!
+    deleteNote(id: ID!): Boolean!
+}
+scalar DateTime `;
