664e7d2063207a3702d4e350ba8c0d3c80f1e0f5
Mayank Kush <mayankkush1734@gmail.com>
2024-10-28 16:36:25 +0530

    Update note.js

--- src/models/note.js
+++ src/models/note.js
@@ -1,22 +1,30 @@
 const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema(
- {
- content: {
- type: String,
- required: true
- },
- author: {
- type: String,
- required: true
- }
- },
- {
- // Assigns createdAt and updatedAt fields with a Date type
- timestamps: true
- }
+    {
+        content: {
+            type: String,
+            required: true
+        },
+        author: {
+            type:mongoose.Schema.Types.ObjectId,
+             ref :"User",
+            required: true
+        },
+        favoriteCount: {
+            type: Number,
+            default: 0
+          },
+  
+          favoritedBy: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: 'User'
+            }]
+    },
+    {
+        timestamps: true
+    }
 );
-// Define the 'Note' model with the schema
 const Note = mongoose.model('Note', noteSchema);
-// Export the module
 module.exports = Note
